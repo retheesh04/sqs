bc53425759a6c079b67261d1e39a8a8c4aefcca0
retheesh04 <109302056+retheesh04@users.noreply.github.com>
2023-11-13 19:08:15 +0530

    Create sqs.tf

---
+++ sqs.tf
@@ -0,0 +1,9 @@
+resource "aws_sqs_queue" "vv-sqs" {
+  name        = var.name
+  visibility_timeout_seconds = var.visibility_timeout_seconds
+  message_retention_seconds  = var.message_retention_seconds
+  max_message_size           = var.max_message_size
+  delay_seconds              = var.delay_seconds
+  receive_wait_time_seconds  = var.receive_wait_time_seconds
+
+}
