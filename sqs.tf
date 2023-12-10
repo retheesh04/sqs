33aae5a2175d05f9f244a62eb7aaa56ef8b091e8
retheesh04 <109302056+retheesh04@users.noreply.github.com>
2023-12-10 21:03:50 +0530

    Update sqs.tf

--- sqs.tf
+++ sqs.tf
@@ -1,9 +1,6 @@
-resource "aws_sqs_queue" "var.title" {
-  name        = var.name
-  visibility_timeout_seconds = var.visibility_timeout_seconds
-  message_retention_seconds  = var.message_retention_seconds
-  max_message_size           = var.max_message_size
-  delay_seconds              = var.delay_seconds
-  receive_wait_time_seconds  = var.receive_wait_time_seconds
+module "sqs-creation1" {
 
+source = "./modules/sqs"
+name = "vv-sqs-clinet-queue1"
 }
+
